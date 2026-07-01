c04980defa1a88a4f365a55e7d84fe71cc715883
makhou <diagnemakhtar2004@gmail.com>
2026-07-01 21:35:55 +0000

    Update project structure

--- src/main/java/org/example/saas_location_terrain/exceptions/StructureNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.saas_location_terrain.exceptions;
-
-public class StructureNotFoundException extends RuntimeException {
-    public StructureNotFoundException(String message) {
-        super(message);
-    }
-}
